232d0f892d3f48484d5cd766dcc567d7efc71800
ysicing <root@ysicing.net>
2018-09-18 00:13:04 +0800

    [REV]fix typo

--- install/salt/salt/install/script/token_check.sh
+++ install/salt/salt/install/script/token_check.sh
@@ -11,7 +11,7 @@ for ((i=1;i<=60;i++ ));do
     sleep 1
     notready=$(curl http://127.0.0.1:7999/login -H "Accept: application/x-yaml" -d username='saltapi' -d password=$secretkey -d eauth='pam' | grep token)
     [ ! -z "$notready" ] && (
-        curl http://127.0.0.1:7999/login -H 'Accept: application/x-yaml' -d username=saltapi -d password=bie5Aiqueiwav5EeY6pae3ahLai3chee -d eauth=pam -s | grep token | awk '{print $2}' > /tmp/.token
+        curl http://127.0.0.1:7999/login -H 'Accept: application/x-yaml' -d username=saltapi -d password=$secretkey -d eauth=pam -s | grep token | awk '{print $2}' > /tmp/.token
     ) && break
 done
 ) 
